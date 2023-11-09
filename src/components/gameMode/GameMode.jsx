87fdada8b2cf9b7fca6ecc4c33ffcbf4c338a747
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2023-11-09 14:56:08 +0900

    Update GameMode component and style

--- src/components/gameMode/GameMode.jsx
+++ src/components/gameMode/GameMode.jsx
@@ -3,6 +3,9 @@ import "./GameMode.scss";
 const GameMode = () => {
   return (
     <div className="gameMode">
+      <div className="title">
+        <img src="/pokemon-title.png" alt="pokemon-title" />
+      </div>
       <div className="btn">
         <button type="button">Easy</button>
         <button type="button">Medium</button>
