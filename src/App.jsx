d3f43f582ed513a82b6a14e2adcb680cf0b06db9
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2023-11-17 22:38:47 +0900

    Refactor App component for better state management

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import GameBoard from "./components/gameBoard/gameBoard";
 import usePokemon from "./utils/usePokemon";
 import { useState } from "react";
 import LoadingScreen from "./components/loadingScreen/LoadingScreen";
+import Score from "./components/score/Score";
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 function App() {
   const [gameStart, setGameStart] = useState(false);
-  const [bestScore, setBestScore] = useState(0);
-  const [gameScore, setGameScore] = useState(0);
   const [gameRound, setGameRound] = useState(0);
   const [gameFinish, setGameFinish] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [cardShowing, setCardShowing] = useState(false);
   const [gameMode, setGameMode] = useState("");
+  const [gameStatus, setGameStatus] = useState("win");
+
+  const [bestScore, setBestScore] = useState(0);
+  const [gameScore, setGameScore] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [cardsShowing, setCardsShowing] = useState(false);
+
   const { shufflePokemonLists, getPokemonLists, pokemons, setPokemons } =
     usePokemon();
 
@@ -31,16 +35,40 @@ function App() {
     setIsLoading(false);
 
     await sleep(CARD_SLEEP_TIME);
-    setCardShowing(true);
+    setCardsShowing(true);
+  };
+
+  const increaseScore = () => {
+    setGameScore((prevScore) => prevScore + 1);
   };
 
   const handleChangeGameMode = (gameMode) => {
     setGameMode(gameMode);
   };
 
-  const handleGameStart = () => {
-    setGameStart(!gameStart);
+  const handleGameStart = (gameLevel) => {
+    setGameRound(gameLevel); // Set gameRound based on gameLevel
+    setGameStart(true);
+    initializePokemons(gameLevel);
+    setGameFinish(false);
   };
+
+  const handleGameRestart = (gameLevel) => {
+    setGameRound(gameLevel); // Set gameRound based on gameLevel
+    setGameStart(true);
+    initializePokemons(gameLevel);
+    setGameFinish(false);
+    setGameScore(0);
+  };
+  const handleGameExit = () => {
+    setGameFinish(false);
+    setGameStart(false);
+    setGameScore(0);
+  };
+  // const handleGameFinish = () => {
+  //   setGameStart(false);
+  //   setGameScore(0);
+  // };
 
   const markCardAsClicked = (index) => {
     const updatedPokemons = [...pokemons];
@@ -49,18 +77,50 @@ function App() {
     console.log(pokemons);
   };
 
+  const handleCardClick = async (cardIndex) => {
+    const card = pokemons[cardIndex];
+    if (card.isClicked) {
+      setGameStatus("Lose");
+      setGameFinish(true);
+      return;
+    }
+
+    markCardAsClicked(cardIndex);
+    increaseScore();
+
+    const everyCardClicked = pokemons.every((card) => card.isClicked);
+    if (!everyCardClicked) {
+      await new Promise((resolve) => setTimeout(resolve, CARD_SLEEP_TIME));
+      setCardsShowing(true);
+      shufflePokemonLists();
+    }
+
+    const isWin = pokemons.length === gameScore;
+    if (isWin) {
+      setGameStatus("Win");
+      setGameFinish(true);
+    }
+  };
+
   return (
     <div className="App">
       {isLoading && <LoadingScreen />}
       {gameStart && !isLoading && (
-        <GameBoard cards={pokemons} gameMode={gameMode} gameScore={gameScore} />
+        <GameBoard
+          cards={pokemons}
+          gameMode={gameMode}
+          gameScore={gameScore}
+          gameRound={gameRound}
+          gameStatus={gameStatus}
+          gameFinish={gameFinish}
+          cardClick={handleCardClick}
+          onRestart={handleGameRestart}
+          onQuit={handleGameExit}
+        />
       )}
       {!gameStart && (
         <GameMode
-          gameOn={(gameLevel) => {
-            handleGameStart();
-            initializePokemons(gameLevel);
-          }}
+          gameOn={handleGameStart}
           handleChangeGameMode={handleChangeGameMode}
         />
       )}
